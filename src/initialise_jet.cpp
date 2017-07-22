04367893f1f64fd7b0bce7bad05c6254ff07788b
Gareth Murphy <garethcmurphy@gmail.com>
2017-07-22 07:44:56 +0200

    Fixing min/max potential macro substitution problem

--- src/initialise_jet.cpp
+++ src/initialise_jet.cpp
@@ -1,8 +1,7 @@
 #include "initialise.h"
 int
-initialise_jet (char *filename, Array3D < zone > grid, int *maxstep,
-		double *cfl)
-{
+initialise_jet(char *filename, Array3D<zone> grid, int *maxstep,
+               double *cfl) {
   int ii = 0;
   int jj = 0;
   int kk = 0;
@@ -11,10 +10,8 @@ initialise_jet (char *filename, Array3D < zone > grid, int *maxstep,
   double rho, ur, vr, wr, pr, bur, bvr, bwr, ar;
   ifstream input;
 
-
   int tempint;
   double tempdouble;
-
 
   double gammam1 = gammag - 1;
   double gammam1i = 1.0 / gammam1;
@@ -25,9 +22,8 @@ initialise_jet (char *filename, Array3D < zone > grid, int *maxstep,
   double sqr4piei = 0;
   double unused = 0;
 
-
-  double pie = acos (-1.0);
-  sqr4pie = 2.0 * sqrt (pie);
+  double pie = acos(-1.0);
+  sqr4pie = 2.0 * sqrt(pie);
   sqr4piei = 1.0 / sqr4pie;
 //  sqr4piei = 1.0 ;
 
@@ -42,69 +38,64 @@ initialise_jet (char *filename, Array3D < zone > grid, int *maxstep,
   vr = 0.0;
   pr = 0.6;
 
-  input.open (filename);
+  input.open(filename);
 
   input >> tempint;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempint;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempint;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempdouble;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempint;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempdouble;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempdouble;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempint;
-  input.ignore (256, '\n');
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
+  input.ignore(256, '\n');
 
   input >> rl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> ul;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> vl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> wl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> pl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bul;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bvl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bwl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
 
   input >> rr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> ur;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> vr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> wr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> pr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bur;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bvr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bwr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
 
-  input.close ();
+  input.close();
 
-
-
-  al = sqrt (gammag * pl / rl);
-  ar = sqrt (gammag * pr / rr);
-
-
-
+  al = sqrt(gammag * pl / rl);
+  ar = sqrt(gammag * pr / rr);
 
   cout << "Ambient state rho= " << rl << "\t Jet State " << rr << endl;
   cout << "Ambient state vx = " << ul << "\t Jet State " << ur << endl;
@@ -120,63 +111,59 @@ initialise_jet (char *filename, Array3D < zone > grid, int *maxstep,
 
 
   /* Initialise the grid array with a shock tube problem  */
-  for (ii = 0; ii < nx; ii++)
-    {
-      for (jj = 0; jj < ny; jj++)
-	{
-	  vx = ul;
-	  vy = vl;
-	  vz = wl;
-	  p = pl;
-	  rho = rl;
-	  b1 = bul * sqr4piei;
-	  b2 = bvl * sqr4piei;
-	  b3 = bwl * sqr4piei;
+  for (ii = 0; ii < nx; ii++) {
+    for (jj = 0; jj < ny; jj++) {
+      vx = ul;
+      vy = vl;
+      vz = wl;
+      p = pl;
+      rho = rl;
+      b1 = bul * sqr4piei;
+      b2 = bvl * sqr4piei;
+      b3 = bwl * sqr4piei;
 
 //   JET
-	  double xdist = ii - nx / 2;
-	  double ydist = jj - ny / 2;
-	  dist = sqrt (xdist * xdist + ydist * ydist);
+      double xdist = ii - nx / 2;
+      double ydist = jj - ny / 2;
+      dist = sqrt(xdist * xdist + ydist * ydist);
 //	  dist = sqrt ((double) (xdist * xdist ));
-	  if (dist < 0.1 * nx )
-	    // if ( ii +jj < 0.5*nx ) 
+      if (dist < 0.1 * nx)
+        // if ( ii +jj < 0.5*nx )
 
-	    {
-	      vx = ur;
-	      vy = vr;
-	      vz = wr;
-	      p = pr;
-	      rho = rr;
-	      b1 = bur * sqr4piei;
-	      b2 = bvr * sqr4piei;
-	      b3 = bwr * sqr4piei;
-	    }
+      {
+        vx = ur;
+        vy = vr;
+        vz = wr;
+        p = pr;
+        rho = rr;
+        b1 = bur * sqr4piei;
+        b2 = bvr * sqr4piei;
+        b3 = bwr * sqr4piei;
+      }
 
-	  bsquared = (b1 * b1 + b2 * b2 + b3 * b3);
-	  grid[ii][jj][kk] _MASS = rho;
-	  grid[ii][jj][kk] _MOMX = rho * vx;
-	  grid[ii][jj][kk] _MOMY = rho * vy;
-	  grid[ii][jj][kk] _MOMZ = rho * vz;
-	  ke = 0.5 * rho * (vx * vx + vy * vy + vz * vz);
-	  grid[ii][jj][kk] _ENER = ke + p * gammam1i + 0.5 * bsquared;
-	  grid[ii][jj][kk] _B_X = b1;
-	  grid[ii][jj][kk] _B_Y = b2;
-	  grid[ii][jj][kk] _B_Z = b3;
-	  if (grid[ii][jj][kk] _ENER < 0.0)
-	    {
-	      cout << "Wtf?" << endl;
-	      exit (0);
-	    }
-	}
-
+      bsquared = (b1 * b1 + b2 * b2 + b3 * b3);
+      grid[ii][jj][kk]_MASS = rho;
+      grid[ii][jj][kk]_MOMX = rho * vx;
+      grid[ii][jj][kk]_MOMY = rho * vy;
+      grid[ii][jj][kk]_MOMZ = rho * vz;
+      ke = 0.5 * rho * (vx * vx + vy * vy + vz * vz);
+      grid[ii][jj][kk]_ENER = ke + p * gammam1i + 0.5 * bsquared;
+      grid[ii][jj][kk]_B_X = b1;
+      grid[ii][jj][kk]_B_Y = b2;
+      grid[ii][jj][kk]_B_Z = b3;
+      if (grid[ii][jj][kk]_ENER < 0.0) {
+        cout << "Wtf?" << endl;
+        exit(0);
+      }
     }
+
+  }
 
   return 0;
 }
 int
-initialise_blast (char *filename, Array3D < zone > grid, int *maxstep,
-		double *cfl)
-{
+initialise_blast(char *filename, Array3D<zone> grid, int *maxstep,
+                 double *cfl) {
   int ii = 0;
   int jj = 0;
   int kk = 0;
@@ -185,10 +172,8 @@ initialise_blast (char *filename, Array3D < zone > grid, int *maxstep,
   double rho, ur, vr, wr, pr, bur, bvr, bwr, ar;
   ifstream input;
 
-
   int tempint;
   double tempdouble;
-
 
   double gammam1 = gammag - 1;
   double gammam1i = 1.0 / gammam1;
@@ -199,9 +184,8 @@ initialise_blast (char *filename, Array3D < zone > grid, int *maxstep,
   double sqr4piei = 0;
   double unused = 0;
 
-
-  double pie = acos (-1.0);
-  sqr4pie = 2.0 * sqrt (pie);
+  double pie = acos(-1.0);
+  sqr4pie = 2.0 * sqrt(pie);
   sqr4piei = 1.0 / sqr4pie;
 //  sqr4piei = 1.0 ;
 
@@ -216,69 +200,64 @@ initialise_blast (char *filename, Array3D < zone > grid, int *maxstep,
   vr = 0.0;
   pr = 0.6;
 
-  input.open (filename);
+  input.open(filename);
 
   input >> tempint;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempint;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempint;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempdouble;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempint;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempdouble;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempdouble;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> tempint;
-  input.ignore (256, '\n');
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
+  input.ignore(256, '\n');
 
   input >> rl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> ul;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> vl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> wl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> pl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bul;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bvl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bwl;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
 
   input >> rr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> ur;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> vr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> wr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> pr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bur;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bvr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
   input >> bwr;
-  input.ignore (256, '\n');
+  input.ignore(256, '\n');
 
-  input.close ();
+  input.close();
 
-
-
-  al = sqrt (gammag * pl / rl);
-  ar = sqrt (gammag * pr / rr);
-
-
-
+  al = sqrt(gammag * pl / rl);
+  ar = sqrt(gammag * pr / rr);
 
   cout << "Ambient state rho= " << rl << "\t Jet State " << rr << endl;
   cout << "Ambient state vx = " << ul << "\t Jet State " << ur << endl;
@@ -294,56 +273,53 @@ initialise_blast (char *filename, Array3D < zone > grid, int *maxstep,
 
 
   /* Initialise the grid array with a shock tube problem  */
-  for (ii = 0; ii < nx; ii++)
-    {
-      for (jj = 0; jj < ny; jj++)
-	{
-	  vx = ul;
-	  vy = vl;
-	  vz = wl;
-	  p = pl;
-	  rho = rl;
-	  b1 = bul * sqr4piei;
-	  b2 = bvl * sqr4piei;
-	  b3 = bwl * sqr4piei;
+  for (ii = 0; ii < nx; ii++) {
+    for (jj = 0; jj < ny; jj++) {
+      vx = ul;
+      vy = vl;
+      vz = wl;
+      p = pl;
+      rho = rl;
+      b1 = bul * sqr4piei;
+      b2 = bvl * sqr4piei;
+      b3 = bwl * sqr4piei;
 
 //   JET
-	  double xdist = ii - nx / 2;
-	  double ydist = jj - ny / 2;
-	  dist = sqrt (xdist * xdist + ydist * ydist);
+      double xdist = ii - nx / 2;
+      double ydist = jj - ny / 2;
+      dist = sqrt(xdist * xdist + ydist * ydist);
 //	  dist = sqrt ((double) (xdist * xdist ));
-	  if (dist < 0.1 * nx )
-	    // if ( ii +jj < 0.5*nx ) 
+      if (dist < 0.1 * nx)
+        // if ( ii +jj < 0.5*nx )
 
-	    {
-	      vx = ur;
-	      vy = vr;
-	      vz = wr;
-	      p = pr;
-	      rho = rr;
-	      b1 = bur * sqr4piei;
-	      b2 = bvr * sqr4piei;
-	      b3 = bwr * sqr4piei;
-	    }
+      {
+        vx = ur;
+        vy = vr;
+        vz = wr;
+        p = pr;
+        rho = rr;
+        b1 = bur * sqr4piei;
+        b2 = bvr * sqr4piei;
+        b3 = bwr * sqr4piei;
+      }
 
-	  bsquared = (b1 * b1 + b2 * b2 + b3 * b3);
-	  grid[ii][jj][kk] _MASS = rho;
-	  grid[ii][jj][kk] _MOMX = rho * vx;
-	  grid[ii][jj][kk] _MOMY = rho * vy;
-	  grid[ii][jj][kk] _MOMZ = rho * vz;
-	  ke = 0.5 * rho * (vx * vx + vy * vy + vz * vz);
-	  grid[ii][jj][kk] _ENER = ke + p * gammam1i + 0.5 * bsquared;
-	  grid[ii][jj][kk] _B_X = b1;
-	  grid[ii][jj][kk] _B_Y = b2;
-	  grid[ii][jj][kk] _B_Z = b3;
-	  if (grid[ii][jj][kk] _ENER < 0.0)
-	    {
-	      cout << "Wtf?" << endl;
-	      exit (0);
-	    }
-	}
-
+      bsquared = (b1 * b1 + b2 * b2 + b3 * b3);
+      grid[ii][jj][kk]_MASS = rho;
+      grid[ii][jj][kk]_MOMX = rho * vx;
+      grid[ii][jj][kk]_MOMY = rho * vy;
+      grid[ii][jj][kk]_MOMZ = rho * vz;
+      ke = 0.5 * rho * (vx * vx + vy * vy + vz * vz);
+      grid[ii][jj][kk]_ENER = ke + p * gammam1i + 0.5 * bsquared;
+      grid[ii][jj][kk]_B_X = b1;
+      grid[ii][jj][kk]_B_Y = b2;
+      grid[ii][jj][kk]_B_Z = b3;
+      if (grid[ii][jj][kk]_ENER < 0.0) {
+        cout << "Wtf?" << endl;
+        exit(0);
+      }
     }
+
+  }
 
   return 0;
 }
--- src/maxspeed.cpp
+++ src/maxspeed.cpp
@@ -1,11 +1,9 @@
 #include "maxspeed.h"
 int
-maxspeed (Array3D < zone > grid, double *maxspeed)
-{
+maxspeed(Array3D<zone> grid, double *maxspeed) {
   int ii = 0;
   int jj = 0;
   int kk = 0;
-
 
   double gammam1 = gammag - 1;
 
@@ -19,12 +17,11 @@ maxspeed (Array3D < zone > grid, double *maxspeed)
    * in the x or y directions and is passed back to maxspeed in the main program
    * and used to work out the time step */
 
-  for (ii = 0; ii < nx - 1; ii++)
-    {
+  for (ii = 0; ii < nx - 1; ii++) {
 #ifdef TWODIM
-      for (jj = 0; jj < ny - 1; jj++)
+    for (jj = 0; jj < ny - 1; jj++)
 #endif /* TWODIM */
-	{
+    {
       double calfven2 = 0;
       double cfast = 0;
       double cfast2 = 0;
@@ -35,70 +32,66 @@ maxspeed (Array3D < zone > grid, double *maxspeed)
       double speed1 = 0;
       double speed2 = 0;
       double speed3 = 0;
-	  rl = grid[ii][jj][kk] _MASS;
-	  px = grid[ii][jj][kk] _MOMX;
-	  py = grid[ii][jj][kk] _MOMY;
-	  pz = grid[ii][jj][kk] _MOMZ;
-	  et = grid[ii][jj][kk] _ENER;
+      rl = grid[ii][jj][kk]_MASS;
+      px = grid[ii][jj][kk]_MOMX;
+      py = grid[ii][jj][kk]_MOMY;
+      pz = grid[ii][jj][kk]_MOMZ;
+      et = grid[ii][jj][kk]_ENER;
 
-	  bu = grid[ii][jj][kk] _B_X;
-	  bv = grid[ii][jj][kk] _B_Y;
-	  bw = grid[ii][jj][kk] _B_Z;
+      bu = grid[ii][jj][kk]_B_X;
+      bv = grid[ii][jj][kk]_B_Y;
+      bw = grid[ii][jj][kk]_B_Z;
 
+      ri = 1.0 / rl;
+      ul = px * ri;
+      vl = py * ri;
+      wl = pz * ri;
+      vv2 = (ul * ul + vl * vl + wl * wl);
+      ke = 0.5 * rl * vv2;
+      bsquared = (bu * bu + bv * bv + bw * bw);
+      pl = et - ke - 0.5 * bsquared;
+      pl = pl * (gammam1);
+      bsquared = bsquared * ri;
 
+      calfven2 = bu * bu * ri;
+      csound2 = gammag * pl / rl;
+      a_star2 = (csound2 + bsquared);
+      term = sqrt(a_star2 * a_star2 - 4 * csound2 * calfven2);
+      cfast2 = 0.5 * (a_star2 + term);
+      cfast = sqrt(cfast2);
 
-	  ri = 1.0 / rl;
-	  ul = px * ri;
-	  vl = py * ri;
-	  wl = pz * ri;
-	  vv2 = (ul * ul + vl * vl + wl * wl);
-	  ke = 0.5 * rl * vv2;
-	  bsquared = (bu * bu + bv * bv + bw * bw);
-	  pl = et - ke - 0.5 * bsquared;
-	  pl = pl * (gammam1);
-	  bsquared = bsquared * ri;
+      speed1 = fabs(ul) + cfast;
+      *maxspeed = (max)(speed1, *maxspeed);
 
+      calfven2 = bv * bv * ri;
+      term = sqrt(a_star2 * a_star2 - 4 * csound2 * calfven2);
+      cfast2 = 0.5 * (a_star2 + term);
+      cfast = sqrt(cfast2);
+      speed2 = fabs(vl) + cfast;
+      *maxspeed = (max)(speed2, *maxspeed);
 
-	  calfven2 = bu * bu * ri;
-		csound2 = gammag * pl / rl;
-		a_star2 = (csound2 + bsquared);
-	  term = sqrt (a_star2 * a_star2 - 4 * csound2 * calfven2);
-	  cfast2 = 0.5 * (a_star2 + term);
-	  cfast = sqrt (cfast2);
-
-		speed1 = fabs (ul) + cfast;
-	  *maxspeed = (max) (speed1, *maxspeed);
-
-	  calfven2 = bv * bv * ri;
-	  term = sqrt (a_star2 * a_star2 - 4 * csound2 * calfven2);
-	  cfast2 = 0.5 * (a_star2 + term);
-	  cfast = sqrt (cfast2);
-	  speed2 = fabs (vl) + cfast;
-	  *maxspeed = (max) (speed2, *maxspeed);
-
-	  calfven2 = bw * bw * ri;
-	  term = sqrt (a_star2 * a_star2 - 4 * csound2 * calfven2);
-	  cfast2 = 0.5 * (a_star2 + term);
-	  cfast = sqrt (cfast2);
-	  speed3 = fabs (wl) + cfast;
-	  *maxspeed = (max) (speed3, *maxspeed);
+      calfven2 = bw * bw * ri;
+      term = sqrt(a_star2 * a_star2 - 4 * csound2 * calfven2);
+      cfast2 = 0.5 * (a_star2 + term);
+      cfast = sqrt(cfast2);
+      speed3 = fabs(wl) + cfast;
+      *maxspeed = (max)(speed3, *maxspeed);
 
 
-	  /*
-	     cout << ii
-	     << " " << jj
-	     << " " << rl 
-	     << " " << px 
-	     << " " << py 
-	     << " " << pl 
-	     << endl;
-	     cout << *maxspeed << endl;
-	     if ( *maxspeed > 700)
-	     exit(0);
-	   */
-	}
+      /*
+         cout << ii
+         << " " << jj
+         << " " << rl
+         << " " << px
+         << " " << py
+         << " " << pl
+         << endl;
+         cout << *maxspeed << endl;
+         if ( *maxspeed > 700)
+         exit(0);
+       */
     }
-
+  }
 
   return 0;
 
