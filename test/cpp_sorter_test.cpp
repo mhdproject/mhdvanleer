ef3cb0a4c5c9c0d954944ed7eb69c6a380a71e35
Gareth Murphy <garethcmurphy@gmail.com>
2017-08-10 19:07:58 +0200

    adding hlld test

--- test/cpp_sorter_test.cpp
+++ test/cpp_sorter_test.cpp
@@ -13,16 +13,16 @@ TEST(sgn_test, int_arr_sort
 
 TEST(riemann_test, int_arr_sort
 ) {
-  double lhs[] = {1, 0, 0, 0, 1, 1, 1, 1};
-  double rhs[] = {1, 0, 0, 0, 1, 1, 1, 1};
+  double lhs[] = {1, 0, 0, 0, 5, 1, 1, 1};
+  double rhs[] = {1, 0, 0, 0, 5, 1, 1, 1};
   double flux[7];
   double res_state[7];
-  double exp_res_state[] = {0, 0, 0, 0, 0, 0, 0, 0};
+  double exp_flux[] = {0, 0, 0, 0, 0, 0, 0, 0};
   int idir = 1;
   Riemann riem;
   double result = riem.solver(lhs, rhs, flux, res_state, idir);
-  EXPECT_FLOAT_EQ(exp_res_state[0], res_state[0]
-  );
+  assert(result == 0);
+  EXPECT_FLOAT_EQ(exp_flux[0], flux[0]);
 }
 
 TEST(hlld_test, int_arr_sort
@@ -30,13 +30,11 @@ TEST(hlld_test, int_arr_sort
   double lhs[] = {1, 0, 0, 0, 1, 1, 1, 1};
   double rhs[] = {1, 0, 0, 0, 1, 1, 1, 1};
   double flux[7];
-  double res_state[7];
-  double exp_res_state[] = {0, 0, 0, 0, 0, 0, 0, 0};
-  int idir = 1;
+  double exp_flux[] = {0, 0, 0, 0, 0, 0, 0, 0};
   Hlld hlld;
   double result = hlld.solver(lhs, rhs, flux);
-  EXPECT_FLOAT_EQ(exp_res_state[0], res_state[0]
-  );
+  assert(result == 0);
+  EXPECT_FLOAT_EQ(exp_flux[0], flux[0]);
 
 }
 
